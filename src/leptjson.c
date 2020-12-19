92eb9d7b35045b5ade6d27ae5a13ed9cc32db907
3019244189 QiChengXiang <18630816527@163.com>
2020-12-19 15:47:35 +0800

    part 2

--- src/leptjson.c
+++ src/leptjson.c
@@ -1,8 +1,16 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
+#include <stdio.h>   /* debug */
+#include <math.h>
+
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+#define ISZERO(ch)          ((ch) == '0')
+#define JUDGEBIT(n)         ((((((unsigned long)(n) & (unsigned long)0x7fffffff) << 1) >> 53) & 1))
+#define JUDGEBITS(n)        (((((unsigned long)(n) & (unsigned long)0x7fffffff) << 1) >> 53) >= 0x07ff)
 
 typedef struct {
     const char* json;
@@ -17,10 +25,10 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-static int lept_parse_basic(lept_context* c, lept_value* v , char* s, short len, int type){
+static int lept_parse_literal(lept_context* c, lept_value* v , char* s, short len, int type){
+    int i = 0;
     EXPECT(c, s[0]);
-    int i;
-    for(i = 0; i < len-1; i++){
+    for(i; i < len-1; i++){
         if(c->json[i] != s[i+1])
             return LEPT_PARSE_INVALID_VALUE;
     }
@@ -33,8 +41,36 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
     char* end;
     /* \TODO validate number */
     v->n = strtod(c->json, &end);
+
     if (c->json == end)
         return LEPT_PARSE_INVALID_VALUE;
+  
+
+    const char* s = c->json;
+    char ch = s[0];
+
+    /*judge the first char*/
+    if((!ISDIGIT(ch)) && ch != '-')
+        return LEPT_PARSE_INVALID_VALUE;
+
+    /*judge the char after '0'*/
+    if(ISZERO(ch) && !(s[1]== '.' || s[1] == 'e' || s[1] == 'E' || s[1] == '\0')){
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+
+    /*judge the least digit after '.'*/
+    while(*s != '.'){
+        s++;
+    }
+    s++;
+    if(!ISDIGIT(*s))
+        return LEPT_PARSE_INVALID_VALUE;
+
+    /*judge the range of the number*/
+    if(isinf(v->n))
+        return LEPT_PARSE_NUMBER_TOO_BIG;
+            
+
     c->json = end;
     v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
@@ -52,9 +88,9 @@ static int lept_judge_singular(lept_context* c, lept_value* v){
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 'n':  return lept_parse_basic(c, v, "null", 4, LEPT_NULL);
-        case 't':  return lept_parse_basic(c, v, "true", 4, LEPT_TRUE);
-        case 'f':  return lept_parse_basic(c, v, "false", 5, LEPT_FALSE);
+        case 'n':  return lept_parse_literal(c, v, "null", 4, LEPT_NULL);
+        case 't':  return lept_parse_literal(c, v, "true", 4, LEPT_TRUE);
+        case 'f':  return lept_parse_literal(c, v, "false", 5, LEPT_FALSE);
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
